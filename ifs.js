fbbf552a6d41fbabed1dbdfee6cb6daa16187b4c
ginabeki <ginabeki3@gmail.com>
2022-02-19 22:42:55 +0300

    learn js

---
+++ ifs.js
@@ -0,0 +1,14 @@
+var first_name = "Ricky";
+var last_name = "Bobby";
+var age = 45;
+var first = true;
+var last = false;
+
+//Falsey
+//false, null, undefined, 0, "", '',NaN
+
+if (age == 45) {
+  //do something if true
+} else {
+  //do something if false
+}
---
+++ var1.js
@@ -0,0 +1,17 @@
+// tell the computer to remember this
+//declaring & assigning
+var name = "Steve"; //DataType Sring
+var id = 234234; //Datatype Number
+var alive = true; //Datatype Boolean
+var xyz; //undefined
+
+//overwriting Assigning new values
+alive = false;
+id = 7;
+name = "John";
+xyz = null;
+
+console.log(name);
+console.log(id);
+console.log(alive);
+console.log(xyz);
